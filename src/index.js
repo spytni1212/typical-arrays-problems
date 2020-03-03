bc8bfeea09c140bbdbb8b3715b3b1d309c69541d
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-03 19:57:43 +0300

    complate task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+    let a = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (a > array[i]) {
+            a = array[i];
+            }
+        }
+    return a;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+    let a = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > a) {
+            a = array[i];
+            }
+        }
+    return a;
+    }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+    let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+    return result = sum / array.length;
+    }
 }
